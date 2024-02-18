b58d0264f824b0537bf48bfdbd581a1d284c0e8a
QuangHuy <nguyenvanquanghuy051102@gmail.com>
2024-02-19 00:00:41 +0700

    update router rooms and house page

--- fe/src/routes/index.jsx
+++ fe/src/routes/index.jsx
@@ -1,28 +1,35 @@
-import {createBrowserRouter} from 'react-router-dom'
-import Dashboard from '../pages/Dashboard/Dashboard'
-import SignUp from '../pages/Auth/SignUp'
-import Login from '../pages/Auth/Login'
-import ToolbarHeader from '../CommonComponents/ToolbarHeader'
-import HousePage from '../pages/Houses/HousePage'
-import Profile from '../pages/Profiles/Profile'
-import RoomsPage from '../pages/Rooms/RoomsPage'
+import { createBrowserRouter } from "react-router-dom";
+import Dashboard from "../pages/Dashboard/Dashboard";
+import SignUp from "../pages/Auth/SignUp";
+import Login from "../pages/Auth/Login";
+import ToolbarHeader from "../CommonComponents/ToolbarHeader";
+import HousePage from "../pages/Houses/HousePage";
+import Profile from "../pages/Profiles/Profile";
+import RoomsPage from "../pages/Rooms/RoomsPage";
 export const appRoutes = createBrowserRouter([
-    {
-        path: '',
-        element: <ToolbarHeader />,
-       children :[
-        {
-            path: '/',
-            element: <Dashboard />
-        },
-        
-        {
-            path: '/Profile',
-            element: <Profile />
-        },
+  {
+    path: "",
+    element: <ToolbarHeader />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
 
-       ]
-    },
+      {
+        path: "/Profile",
+        element: <Profile />,
+      },
+      {
+        path: "/house",
+        element: <HousePage />,
+      },
+      {
+        path: "/rooms",
+        element: <RoomsPage />,
+      },
+    ],
+  },
 
   {
     path: "/login",
@@ -31,13 +38,5 @@ export const appRoutes = createBrowserRouter([
   {
     path: "/register",
     element: <SignUp />,
-  },
-  {
-    path: "/house",
-    element: <HousePage />,
-  },
-  {
-    path: "/rooms",
-    element: <RoomsPage />,
   },
 ]);
