e831ebfdc949f3b1751becc2a3a2f93ddbea4ce1
QuangHuy <nguyenvanquanghuy051102@gmail.com>
2024-02-22 20:20:53 +0700

    add house service

--- fe/src/pages/Houses/HousePage.jsx
+++ fe/src/pages/Houses/HousePage.jsx
@@ -2,7 +2,7 @@ import DenseTable from "./Component/Table";
 import BasicModal from "./Component/Popup";
 const HousePage = () => {
   return (
-    <div>
+    <div  style={{ backgroundColor: "#F1F2F7", padding: "20px", width: "100%" }}>
       <div className="d-flex">
         <BasicModal />
       </div>
