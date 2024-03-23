c2935cce1e517c9d2ab770f361df25d4c228a151
Nguyen Tran <trantrungnguyenad@gmail.com>
2024-03-23 16:24:39 +0700

    validate same number room

--- be/src/services/Room.service.js
+++ be/src/services/Room.service.js
@@ -15,55 +15,72 @@ const RoomService = {
             const buffer = req.file.buffer;
             await workbook.xlsx.load(buffer);
             const worksheet = workbook.worksheets[0];
-
+    
             const salt = await bcrypt.genSalt(10);
             const hashedPassword = await bcrypt.hash("Rms@12345", salt);
-
-            for (
-                let rowNumber = 1;
-                rowNumber <= worksheet.rowCount;
-                rowNumber++
-            ) {
+    
+            const rooms = await Rooms.find({houseId,deleted:false}); 
+            const existingRoomNames = []
+            rooms.map(room => {
+                existingRoomNames.push(room.name);
+            })
+            console.log(existingRoomNames);
+    
+            for (let rowNumber = 1; rowNumber <= worksheet.rowCount; rowNumber++) {
                 const row = worksheet.getRow(rowNumber);
                 if (rowNumber !== 1 && row.getCell(1).value) {
-                    const floor = row
-                        .getCell(1)
-                        .value.toString()
-                        .trim()
-                        .charAt(0);
-                    const rowData = await Rooms.create({
-                        floor: floor,
-                        name: row.getCell(1).value,
-                        status: row.getCell(2).value,
-                        quantityMember: row.getCell(3).value,
-                        roomType: row.getCell(4).value,
-                        roomPrice: row.getCell(5).value,
-                        deposit: row.getCell(6).value,
-                        area: row.getCell(7).value,
-                        houseId: houseId,
-                        utilities: house?.utilities || [],
-                        otherUtilities: house?.otherUtilities || [],
-                    });
-                    const accountData = await AccountModel.create({
-                        username:
-                            house.name.replace(/\s/g, "") +
-                            row.getCell(1).value,
-                        password: hashedPassword,
-                        accountType: "renter",
-                        roomId: rowData.id,
-                        status: false,
-                    });
-                    house.numberOfRoom += 1;
-                    await house.save();
+                    const roomName = row.getCell(1).value.toString().trim();
+                    
+                    if (existingRoomNames.includes(roomName)) {
+                        throw new Error(`Phòng "${roomName}" đã tồn tại.`)
+                        continue; 
+                    }
+    
+                    existingRoomNames.push(roomName); 
+                }
+            }
+    
+            for (let rowNumber = 1; rowNumber <= worksheet.rowCount; rowNumber++) {
+                const row = worksheet.getRow(rowNumber);
+                if (rowNumber !== 1 && row.getCell(1).value) {
+                    const roomName = row.getCell(1).value.toString().trim();
+                    const floor = roomName.charAt(0);
+                    
+                    // Kiểm tra xem tên phòng đã được kiểm tra và không trùng lặp
+                    if (!existingRoomNames.includes(roomName)) {
+                        const rowData = await Rooms.create({
+                            floor: floor,
+                            name: roomName,
+                            status: row.getCell(2).value,
+                            quantityMember: row.getCell(3).value,
+                            roomType: row.getCell(4).value,
+                            roomPrice: row.getCell(5).value,
+                            deposit: row.getCell(6).value,
+                            area: row.getCell(7).value,
+                            houseId: houseId,
+                            utilities: house?.utilities || [],
+                            otherUtilities: house?.otherUtilities || [],
+                        });
+                        const accountData = await AccountModel.create({
+                            username: house.name.replace(/\s/g, "") + roomName,
+                            password: hashedPassword,
+                            accountType: "renter",
+                            roomId: rowData.id,
+                            status: false,
+                        });
+                        house.numberOfRoom += 1;
+                        await house.save();
+                    }
                 }
             }
             return {
                 message: "oke",
             };
         } catch (error) {
-            console.log(error);
+            throw error
         }
     },
+    
 
     addOne: async (req) => {
         try {
